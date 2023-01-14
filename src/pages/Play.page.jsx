f1b0bf06d875d94bfbf57df1f51ce0ca63237771
Beast1610 <singhharsan@gmail.com>
2023-01-14 20:15:57 +0530

    Some changes to plays page

--- src/pages/Play.page.jsx
+++ src/pages/Play.page.jsx
@@ -25,7 +25,16 @@ const PlayPage = () => {
                 <Poster
                   isPlay={true}
                   src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-suhani-shah-kahaani-india-tour-0-2022-8-29-t-12-36-14.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
+                  title="Suhani Shah kahaani-India Tour"
+                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
+                />
+              </div>
+             
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-sunburn-arena-ft-dj-snake-delhi-ncr-0-2022-8-19-t-11-31-17.jpg"
+                  title="Sunburn Arena Tour Ft. DJ Snake"
                   subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
                 />
               </div>
@@ -33,37 +42,12 @@ const PlayPage = () => {
                 <Poster
                   isPlay={true}
                   src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-daddy-kool-by-atul-khatri-0-2022-8-16-t-10-37-17.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
-                />
-              </div>
-              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
-                <Poster
-                  isPlay={true}
-                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-sunburn-arena-ft-dj-snake-delhi-ncr-0-2022-8-19-t-11-31-17.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
-                />
-              </div>
-              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
-                <Poster
-                  isPlay={true}
-                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-so-rude-of-me-by-swati-sachdeva-0-2022-9-4-t-6-10-37.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
-                />
-              </div>
-              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
-                <Poster
-                  isPlay={true}
-                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-so-rude-of-me-by-swati-sachdeva-0-2022-9-4-t-6-10-37.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
+                  title="Daddy Kool- A standup show by Atul Khatri"
                   subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
                 />
               </div>
             </div>
           </div>
-
           <div className="lg:w-1/4 p-4 bg-white rounded">
             <h2 className="text-2xl font-bold mb-4">Filters</h2>
             <div>
